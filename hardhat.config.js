9e75339e7e1a1e94bb2fcc4380d5e110ff661ac5
Nikita <nvadeveloper@gmail.com>
2022-04-09 21:25:13 +0300

    NFT and NFTMarket contract

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+
+const fs = require('fs')
+const privateKey = fs.readFileSync('.secret').toString()
+
+const projectId = '1839705833d340189785777890b5544a'
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+  },
+  solidity: "0.8.4",
+};
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+  <div>
+    Home
+  </div>
+}
+
+export default Home
